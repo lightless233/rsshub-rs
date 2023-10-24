6f6a1b43255346416a22730c417f381e594ef092
lightless <lightless@foxmail.com>
2023-10-24 17:45:24 +0800

    add index

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ mod service;
 
 use crate::service::{pyn3rd, secrss};
 
+async fn index() -> String {
+    return "ok".to_owned();
+}
+
 #[tokio::main]
 async fn main() {
     // 启动 Web 服务
     let web_app = axum::Router::new()
+        .route("/", axum::routing::get(index))
         .route("/secrss", axum::routing::get(secrss))
         .route("/pyn3rd", axum::routing::get(pyn3rd));
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
